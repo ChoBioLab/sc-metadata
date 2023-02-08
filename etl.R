2638b0838611c45f53b1db62dfda2cff55db14e6
Christopher Tastad <ctastad@gmail.com>
2023-02-08 15:37:28 -0500

    achieved milestone for standard sample id based transformation

--- etl.R
+++ etl.R
@@ -1,9 +1,29 @@
 library(dplyr)
 
-length(select(a, contains("standard_sample_id")))
+raw <- read.csv("input.csv")
 
-a <- read.csv('input.csv')
+standard_id_count <- length(
+  select(
+    raw,
+    contains("standard_sample_id")
+  )
+)
 
-b <- a %>%
-    select(!matches("_[0-9]$") | matches("_2$"))
+for (i in 1:standard_id_count) {
+  tmp <- select(
+    raw,
+    !matches("_[0-9]$") | matches(paste0("_", i, "$")) & !matches("__")
+  )
 
+  colnames(tmp) <- gsub(
+    pattern = paste0("_", i, "$"),
+    replacement = "",
+    x = colnames(tmp)
+  )
+
+  if (exists("output")) {
+    rbind(output, tmp)
+  } else {
+    output <- tmp
+  }
+}
