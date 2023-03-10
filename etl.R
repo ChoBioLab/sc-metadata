3868cfdb68beee4c00d823b5ebaf41befb123d27
Christopher Tastad <ctastad@gmail.com>
2023-03-10 18:28:54 -0500

    implement better bind_rows function for library ID based transform

--- etl.R
+++ etl.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 
 raw <- read.csv("input.csv")
+raw[is.na(raw)] <- ""
+raw <- as_tibble(raw)
 
 lib_prep_raw <- select(raw, !c(lib_id_seq_1:length(raw)))
 
@@ -19,7 +21,7 @@ for (i in 1:lib_id_count) {
     !matches("[a-z]_[0-9]$") |
       matches(paste0("[a-z]_", i, "$"))
   ) %>%
-    filter(redcap_event_name == "forms_arm_1")
+    filter(redcap_event_name == "Forms (Arm 1: Sample Info and Library Prep)")
 
   colnames(tmp) <- gsub(
     pattern = paste0("_", i, "$"),
@@ -40,7 +42,12 @@ for (i in 1:lib_id_count) {
   ) %>%
     mutate(
       lib_type = "ctyo-gex"
-    )
+    ) %>%
+    rename_with(., ~ gsub(
+      pattern = "cyto_gex_|_cyto_gex",
+      replacement = "",
+      .x
+    ))
 
   nuc <- select(
     tmp,
@@ -54,7 +61,12 @@ for (i in 1:lib_id_count) {
   ) %>%
     mutate(
       lib_type = "nuc-gex"
-    )
+    ) %>%
+    rename_with(., ~ gsub(
+      pattern = "nuc_gex_|_nuc_gex",
+      replacement = "",
+      .x
+    ))
 
   atac <- select(
     tmp,
@@ -67,60 +79,34 @@ for (i in 1:lib_id_count) {
   ) %>%
     mutate(
       lib_type = "atac"
-    )
-
-  # cleanup column name strings specific to lib type
-  colnames(cyto) <- gsub(
-    pattern = "cyto_gex_|_cyto_gex",
-    replacement = "",
-    x = colnames(cyto)
-  )
-
-  colnames(nuc) <- gsub(
-    pattern = "nuc_gex_|_nuc_gex",
-    replacement = "",
-    x = colnames(nuc)
-  )
-
-  colnames(atac) <- gsub(
-    pattern = "atac_",
-    replacement = "",
-    x = colnames(atac)
-  )
-
-  colnames(atac) <- gsub(
-    pattern = "id_atac",
-    replacement = "id",
-    x = colnames(atac)
-  )
+    ) %>%
+    rename_with(., ~ gsub(
+      pattern = "atac_",
+      replacement = "",
+      .x
+    )) %>%
+    rename_with(., ~ gsub(
+      pattern = "id_atac",
+      replacement = "id",
+      .x
+    ))
 
   # append rows to final table
   if (exists("final_table")) {
-      print(colnames(atac))
-      print(length(atac))
-      print(length(nuc))
-      print(length(cyto))
-      print(colnames(cyto))
-    final_table <- rbind(
+    final_table <- bind_rows(
       final_table,
       cyto,
       nuc,
       atac
     )
   } else {
-      print(colnames(atac))
-      print(length(atac))
-      print(length(nuc))
-      print(length(cyto))
-      print(colnames(cyto))
-    final_table <- rbind(
+    final_table <- bind_rows(
       cyto,
       nuc,
       atac
     )
   }
 }
-
 
 is.na(final_table) <- final_table == ""
 final_table <- final_table %>%
@@ -168,6 +154,7 @@ final_table <- final_table %>%
       type_of_experiment___spatialffpe,
       type_of_experiment___spatialoct,
       x_chem_version_sc,
+      no_samples_di,
       no_of_samples_3_prime,
       x_chem_version_2_ma,
       no_of_samples_multiome,
@@ -175,6 +162,10 @@ final_table <- final_table %>%
       sample_information_complete,
       cdna_date,
       pcr_cycles,
+      cdna_conc,
+      cdna_conc_dilution,
+      total_cdna_yield,
+      input_cdna,
       index_used,
       lib_conc,
       lib_dilution,
@@ -190,8 +181,7 @@ final_table <- final_table %>%
       novaseq_6000,
       novaseq_2000,
       lanes_full_flow_cell,
-      number_of_lanes,
-      no_samples_di
+      number_of_lanes
     )
   ) %>%
   select(
